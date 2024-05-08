affb175310499a63ab22e9093c96c29e3bb4c44a
Zhagrin <Z0rr083@mail.ru>
2024-05-08 15:31:00 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+#immutable_var = (23,17,'coffee','HTTP')
+#print(immutable_var)
+#immutable_var[1]=38 # кортеж - неизменяемый тип данных
+mutable_list=["good","yellow","crew",22,8] # список - изменяемый тип данных
+mutable_list[1]='red'
+mutable_list[3]=165
+print(mutable_list)
